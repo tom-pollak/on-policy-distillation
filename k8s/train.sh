8c350a3ff735d43881bc763e740c153979b3489f
Tom Pollak <tompollak1000@gmail.com>
2025-12-18 19:18:53 +0100

    train.sh

---
+++ k8s/train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+job_name="$1"
+shift
+
+cmd="
+set -euo pipefail
+curl -LsSf https://astral.sh/uv/install.sh | sh
+cd /data/tomp/on-policy-distillation/
+source .env
+uv sync
+uv run accelerate launch train.py --output_dir $job_name $@
+"
+
+JOB_NAME=$job_name krun --gpu 8 --priority low --run-command "$cmd"
